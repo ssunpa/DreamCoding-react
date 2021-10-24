fbbd9f7a251521bf526952530aa952dd7722179c
ssunpa <bestpa.1130@gmail.com>
2021-10-24 19:03:03 +0900

    CH04 Habit-tracker, Add : add form, reset button

--- src/app.jsx
+++ src/app.jsx
@@ -33,6 +33,22 @@ class App extends Component {
         this.setState({ habits });
     };
 
+    handleAdd = (name) => {
+        const habits = [
+            ...this.state.habits,
+            { id: Date.now(), name, count: 0 },
+        ];
+        this.setState({ habits });
+    };
+
+    handleReset = () => {
+        const habits = this.state.habits.map((habit) => {
+            habit.count = 0;
+            return habit;
+        });
+        this.setState({ habits });
+    };
+
     render() {
         return (
             <>
@@ -47,6 +63,8 @@ class App extends Component {
                     onIncrement={this.handleIncrement}
                     onDecrement={this.handleDecrement}
                     onDelete={this.handleDelete}
+                    onAdd={this.handleAdd}
+                    onReset={this.handleReset}
                 />
             </>
         );
